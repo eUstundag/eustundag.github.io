e4be08cd7e3e61a5a81cbf8bf78866112343ddf7
smokecode <smokecode@gmail.com>
2015-11-08 12:26:11 -0500

    event listeners for clicks

--- site.js
+++ site.js
@@ -1,3 +1,19 @@
-$(function(){
+function toggleVisibility(target){
+  if ($(target).css('visibility') === 'hidden'){
+      $(target).css('visibility', 'visible');
+    } else {
+      $(target).css('visibility', 'hidden');
+    }
+}
 
+$(function(){
+  $('#blog_header').click(function(){
+    toggleVisibility('#blog_links');
+  });
+  $('#contact_header').click(function(){
+    toggleVisibility('#contact_links');
+  });
+  $('#project_header').click(function(){
+    toggleVisibility('#project_links');
+  });
 });
